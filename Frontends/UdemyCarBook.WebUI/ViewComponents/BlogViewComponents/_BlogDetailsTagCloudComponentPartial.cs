2707234279d9e2d037b98d2a211e3344e934c1cd
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2025-10-21 01:53:42 +0300

    Completed Get TagClouds With BlogId in BlogDetail Page

--- Frontends/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
@@ -16,11 +16,11 @@ namespace UdemyCarBook.WebUI.ViewComponents.BlogViewComponents
 		public async Task<IViewComponentResult> InvokeAsync(int id)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync($"https://localhost:7095/api/TagClouds" + id);
+			var responseMessage = await client.GetAsync($"https://localhost:7095/api/TagClouds/GetTagCloudByBlogId?id=" + id);
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<GetByBlogIdTagCloudDto>(jsonData);
+				var values = JsonConvert.DeserializeObject<List<GetByBlogIdTagCloudDto>>(jsonData);
 				return View(values);
 			}
 			return View();
