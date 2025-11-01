f709238e9513e530f473812559735696787638af
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2025-11-01 22:39:26 +0300

    Added AdminCategory Controller in the part of Area

---
+++ Frontends/UdemyCarBook.Dto/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Dto.CategoryDtos
+{
+	public class UpdateCategoryDto
+	{
+		public int CategoryID { get; set; }
+
+		public string CategoryName { get; set; }
+	}
+}
