34e745c2157346f026ab9e22017205cb15388f7c
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2026-01-17 23:37:54 +0300

    Created Register features with Mediator design pattern

---
+++ CarBookProject.Application/Features/Mediator/Handlers/AppUserHandler/CreateAppUserCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using CarBook.Domain.Entities;
+using CarBookProject.Application.Enums;
+using CarBookProject.Application.Features.Mediator.Commands.RegisterCommands;
+using CarBookProject.Application.Interfaces;
+using CarBookProject.Application.Interfaces.AppUserInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookProject.Application.Features.Mediator.Handlers.AppUserHandler
+{
+	public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+	{
+		private readonly IRepository<AppUser> _repository;
+
+		public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.CreateAsync(new AppUser
+			{
+				Password = request.Password,
+				UserName = request.Username,
+				AppRoleId = (int)RolesType.Member
+			});
+		}
+	}
+}
---
+++ Presentation/CarBookProject.WebApi/Controllers/RegistersController.cs
@@ -0,0 +1,27 @@
+﻿using CarBookProject.Application.Features.Mediator.Commands.RegisterCommands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegistersController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public RegistersController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateUser(CreateAppUserCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Kullanıcı Oluşturuldu.");
+
+		}
+	}
+}
